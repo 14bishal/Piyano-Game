b0ae856043478c38e6c7d40819af079c68276e54
14bishal <64361639+14bishal@users.noreply.github.com>
2023-12-12 00:18:07 +0530

    Initial commit

---
+++ components/index.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+const GRID_BOX = 12;
+
+function PianoComponent() {
+  const [selectedBox, setSelectedBox] = useState([]);
+  const [select, setSelect] = useState(null);
+
+  const playBox = (val, idx) => {
+    setTimeout(() => {
+      setSelect(val);
+    }, idx * 1000);
+  };
+
+  const handlePlay = () => {
+    selectedBox?.map((itm, idx) => playBox(itm, idx));
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSelect(null);
+    }, 100);
+  }, [select]);
+
+  return (
+    <>
+      <div className="container">
+        {[...Array(GRID_BOX).keys()].map((key) => (
+          <div
+            key={key}
+            className={`${'each_box_container'} ${
+              select !== key ? '' : 'blink_box'
+            }`}
+            onClick={() => {
+              setSelect(key);
+              setSelectedBox((prev) => [...prev, key]);
+            }}
+          >
+            {key}
+          </div>
+        ))}
+      </div>
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={() => setSelectedBox([])}>Reset</button>
+    </>
+  );
+}
+
+export default PianoComponent;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import PianoComponent from '../components';
+
+import './style.css';
+
+export const App: FC<{ name: string }> = ({ name }) => {
+  return (
+    <div>
+      <h1>Piyano Game</h1>
+      <p>Start clicking on the box to see some magic happen :)</p>
+      <PianoComponent />
+    </div>
+  );
+};
